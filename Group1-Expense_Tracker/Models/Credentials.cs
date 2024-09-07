0ffc2da16ebdd62672952d93956ee4a2611db626
Alvin John Araneta <alvinjohnaranetz@gmail.com>
2024-09-07 08:23:50 +0800

    Need to fix register

--- Group1-Expense_Tracker/Models/Credentials.cs
+++ Group1-Expense_Tracker/Models/Credentials.cs
@@ -3,15 +3,16 @@ using System.ComponentModel.DataAnnotations;
 namespace Group1_Expense_Tracker.Models
 {
 	public class Credentials
-	{
-		[Required]
-		[EmailAddress]
-		public string EmailAdd { get; set;}
+{
+    [Required(ErrorMessage = "Username is required.")]
+    public string Username { get; set; }
 
-		[Required]
-		public string Password { get; set;}
+    [Required(ErrorMessage = "Email address is required.")]
+    [EmailAddress(ErrorMessage = "Invalid email format.")]
+    public string EmailAdd { get; set; }
 
-		[Required]
-		public string Username { get; set; }
-	}
+    [Required(ErrorMessage = "Password is required.")]
+    [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long.")]
+    public string Password { get; set; }
+}
 }
